dd507ff61243bab50600b181263f14594d9d1f2a
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-06-12 01:24:55 +0530

    Updated getStarted.dart (get Started UI ready)+ updated main.dart

--- homerental_ui/lib/getStarted.dart
+++ homerental_ui/lib/getStarted.dart
@@ -1,29 +1,75 @@
-// ignore_for_file: camel_case_types
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
-class getStarted extends StatefulWidget {
-  const getStarted({super.key});
+class Getstarted extends StatefulWidget {
+  const Getstarted({super.key});
 
   @override
-  State createState() => _getStartedState();
+  State createState() => _GetstartedState();
 }
 
-class _getStartedState extends State {
+class _GetstartedState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.tealAccent,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/bg_img1.jpg"),
+          Expanded(
+            flex: 7, // Adjust the flex values as needed
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/bg_img1.jpg"),
+                ),
               ),
+            ),
+          ),
+          Expanded(
+            flex: 3, // Adjust the flex values as needed
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Let's Find Your Paradise",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Find your perfect dream space with just a few clicks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      Colors.blue,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 40.0,
+                    ),
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],

86b75895411e0f11b82f553db3b15a3790ebfea8
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-29 10:50:06 +0530

    Added img

--- plantapp_ui/lib/detailScreen.dart
+++ plantapp_ui/lib/detailScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -10,6 +11,36 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 40),
+        child: Column(
+          children: [
+            //Row 1: back arrow key
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    log("Icon Tapped");
+                  },
+                  child: const Icon(Icons.arrow_back),
+                ),
+              ],
+            ),
+            //image Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/snakePlant.png",
+                  width: 195,
+                  height: 243,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

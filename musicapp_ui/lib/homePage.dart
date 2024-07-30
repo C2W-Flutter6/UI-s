9b972d203d02a955429901a9a5030b59f03ad160
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-30 12:34:53 +0530

    background img set

--- musicapp_ui/lib/homePage.dart
+++ musicapp_ui/lib/homePage.dart
@@ -7,33 +7,58 @@ class Home_page extends StatefulWidget {
   State createState() => _Home_pageState();
 }
 
-class _Home_pageState extends State {
+class _Home_pageState extends State<Home_page> {
   @override
   Widget build(BuildContext context) {
+    // Get the screen height
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            flex: 8,
+          SizedBox(
+            height: screenHeight *
+                0.75, // Set the height to 60% of the screen height
             child: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage(
-                    "assets/img1.jpg",
-                  ),
+                  image: AssetImage("assets/img1.jpg"),
+                  fit: BoxFit.cover, // Ensure the image covers the container
                 ),
               ),
-              child: const SizedBox(
-                width: 314,
-                height: 132,
-                child: Text(
-                  "Dancing between The shadows Of rhythm",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w600),
-                ),
+              child: Stack(
+                //Text 1 : Dancing between the shadows....
+                children: [
+                  Positioned(
+                    top: 400,
+                    child: Text(
+                      "Dancing between\n The shadows \nOf rhythm ",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  //Button 1: Get started
+                  Positioned(
+                    top: 420,
+                    child: Text(
+                      "Second text",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
+          ),
+          // Button 2:Continue with email
+          Container(
+            width: 20,
+            height: 20,
+            color: Colors.blueAccent,
           ),
         ],
       ),
